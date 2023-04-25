8fc9f785ecabc8f339435df3d96ed9882c16de5f
heroraz <raz@herolo.co.il>
2023-04-25 11:15:36 +0300

    added initial route pages for basel and geneva

---
+++ src/app/(pages)/basel/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.scss";
+
+export default async function BaselPage() {
+  return <main className={styles.baselPage}></main>;
+}
---
+++ src/app/(pages)/geneva/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.scss";
+
+export default async function GenevaPage() {
+  return <main className={styles.genevaPage}></main>;
+}
