5ae4bdad17ad391fa01493268e60e83761359300
heroraz <raz@herolo.co.il>
2023-04-25 11:28:07 +0300

    updated pages to show the radio buttons and sample a fetch

--- src/app/(pages)/basel/page.tsx
+++ src/app/(pages)/basel/page.tsx
@@ -1,5 +1,13 @@
+import { getData } from "@/app/utils/serverOnlyFunctions";
 import styles from "./page.module.scss";
+import ContentContainer from "@/app/components/ClientComponents/ContentContainer/ContentContainer";
 
 export default async function BaselPage() {
-  return <main className={styles.baselPage}></main>;
+  const data = await getData("departure");
+
+  return (
+    <main className={styles.baselPage}>
+      <ContentContainer data={data} />
+    </main>
+  );
 }
--- src/app/(pages)/geneva/page.tsx
+++ src/app/(pages)/geneva/page.tsx
@@ -1,5 +1,13 @@
+import { getData } from "@/app/utils/serverOnlyFunctions";
 import styles from "./page.module.scss";
+import ContentContainer from "@/app/components/ClientComponents/ContentContainer/ContentContainer";
 
 export default async function GenevaPage() {
-  return <main className={styles.genevaPage}></main>;
+  const data = await getData("departure");
+
+  return (
+    <main className={styles.genevaPage}>
+      <ContentContainer data={data} />
+    </main>
+  );
 }
